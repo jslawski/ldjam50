65d66094c425502072b98cde787334dd76ab1f06
ruddgasm <45502739+ruddgasm@users.noreply.github.com>
2022-04-03 01:18:48 -0700

    Initial Level Setup
    easy level setup with pieces ready to go

---
+++ Assets/3D Assets/FMScreenDepthNormal.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class FMScreenDepthNormal : MonoBehaviour
+{
+    public Camera Cam;
+    public Material Mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cam == null)
+        {
+            Cam = this.GetComponent<Camera>();
+            Cam.depthTextureMode = DepthTextureMode.DepthNormals;
+        }
+
+        if(Mat == null)
+        {
+            Mat = new Material(Shader.Find("Hidden/FMShader_ScreenDepthNormal"));
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //Graphics.Blit(source, destination);
+
+        Graphics.Blit(source, destination, Mat);
+    }
+}
